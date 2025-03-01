fbbecc7d6ed3caf32214ab2ae70add5fbeb290fe
Mohammad Mehedi Hasan <mohammadmehedihasan84@gmail.com>
2025-03-02 02:37:37 +0600

    login page design

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../layouts/MainLayouts";
-import Home from "../Pages/Home";
+import Home from "../pages/Home";
 import Register from "../pages/Register";
 import Login from "../pages/login";
 import ContactUs from "../pages/ContactUs";
