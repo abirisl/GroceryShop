27f874e1eddae0f2498aa9860e438e1f42e987ca
Mohammad Mehedi Hasan <mohammadmehedihasan84@gmail.com>
2025-03-05 02:21:17 +0600

    login page responsive

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function login() {
   return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+      <div className="md:min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="bg-white p-5 rounded-lg shadow-lg w-full max-w-md">
           <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Sign In</h2>
           <form>
             {/* Email Input */}
